510d350aa20bc1c6275ea4abc14a8c208cc0902b
Elmi05 <karshe0548@gmail.com>
2023-10-30 19:20:14 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+function calculateBMI(weight, height) {
+    return weight / (height * height)
+}
+
+let aliWeight = 50;
+let aliHeight = 1.83;
+let aliBMI = calculateBMI(aliWeight, aliHeight)
+
+let mohamedWeight = 60;
+let mohamedHeight = 1.80;
+let mohamedBMI = calculateBMI(mohamedWeight, mohamedHeight)
+
+if (aliBMI > mohamedBMI) {
+    console.log("Ali BMI is higher")
+} else if (mohamedBMI > aliBMI) {
+    console.log("Mohamed BMI is higher")
+} else {
+    console.log("Ali and Mohamed have the same BMI")
+}
